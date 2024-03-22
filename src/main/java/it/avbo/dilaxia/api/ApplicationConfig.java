863bc3deb4dd30a7d12d57b6d9f1451de3bb14c7
aintDatCap <caprinifede@gmail.com>
2024-03-22 17:12:33 +0100

    [DN-4] Creazione del template

---
+++ src/main/java/it/avbo/dilaxia/api/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package it.avbo.dilaxia.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+
+    public ApplicationConfig() {
+    }
+}
