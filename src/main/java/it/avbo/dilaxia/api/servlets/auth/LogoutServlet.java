d3a552b4c37c38150e9c3314382c8363ca603c0a
Leprots <lor3skate11@gmail.com>
2024-03-27 19:27:44 +0100

    [DN-8] Servlet per il logout degli utenti

---
+++ src/main/java/it/avbo/dilaxia/api/servlets/auth/LogoutServlet.java
@@ -0,0 +1,22 @@
+package it.avbo.dilaxia.api.servlets.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/auth/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.isRequestedSessionIdValid()) {
+            req.logout();
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
