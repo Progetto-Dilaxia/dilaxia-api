f701cf162c0735503d6a499fcdb2fcd7c5f06fcf
aintDatCap <federico.caprini@protonmail.com>
2024-04-20 18:06:08 +0200

    [DN-28] Servlet per l'eliminazione dell'account

---
+++ src/main/java/it/avbo/dilaxia/api/models/auth/AccountDeletionModel.java
@@ -0,0 +1,9 @@
+package it.avbo.dilaxia.api.models.auth;
+
+public class AccountDeletionModel {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+}
