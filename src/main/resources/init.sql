54a63d519b2547d4b8e76adcc2f1b5c11496dfd6
BethDC <150241378+BethDC@users.noreply.github.com>
2024-04-22 16:16:34 +0200

    [DN-14] Implementazione delle tabelle SQL

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -11,25 +11,34 @@ CREATE TABLE IF NOT EXISTS utenti (
 
 --Per il tipo: può essere Terra (T), Sintetico (S), Cemento (C)
 CREATE TABLE IF NOT EXISTS campi (
-	id smallint(4) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	id smallint UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	indirizzo varchar(50) NOT NULL,
-    tipo char(1) NOT NULL
+    	tipo varchar(1) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS squadre(
+	id smallint UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(40) NOT NULL,
+	id_sport smallint UNSIGNED NOT NULL,
+	username_coach varchar(30) NOT NULL,
+	FOREIGN KEY (id_sport) REFERENCES sport(id),
+	FOREIGN KEY (username_coach) REFERENCES utenti(username)
 );
 
 CREATE TABLE IF NOT EXISTS sport (
-	id smallint(4) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	id smallint UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	nome_sport varchar(30) NOT NULL,
 	descrizione varchar(256)
 );
 
 CREATE TABLE IF NOT EXISTS tornei (
 	id int PRIMARY KEY AUTO_INCREMENT,
-	id_sport smallint(4) UNSIGNED NOT NULL,
-	id_campo smallint(4) UNSIGNED NOT NULL,
+	id_sport smallint UNSIGNED NOT NULL,
+	id_campo smallint UNSIGNED NOT NULL,
 	username_creatore varchar(30) NOT NULL,
 	descrizione varchar(256),
-    data_inizio DATE,
-    data_fine DATE,
+    	data_inizio DATE,
+    	data_fine DATE,
 	FOREIGN KEY (id_sport) REFERENCES sport(id),
 	FOREIGN KEY (id_campo) REFERENCES campo(id),
 	FOREIGN KEY (username_creatore) REFERENCES utenti(username)
@@ -37,57 +46,65 @@ CREATE TABLE IF NOT EXISTS tornei (
 );
 
 --con anni_classi_partecipanti si intende Biennio (B), Triennio (T) o Misto (M), per genere si intende che il torneo è dedicato a Maschi (M), Femmine (F), o misto (M).
-CREATE TABLE IF NOT EXISTS partite_scolastiche (
+CREATE TABLE IF NOT EXISTS partite(
 	id int PRIMARY KEY AUTO_INCREMENT,
-	id_campo smallint(4) UNSIGNED NOT NULL,
+	id_campo smallint UNSIGNED NOT NULL,
 	username_creatore varchar(30) NOT NULL,
 	descrizione varchar(256),
-    data_partita DATE,
-    orario timestamp,
-    anni_classe_partecipanti char(1),
-    max_giocatori tinyint(4),
-    min_giocatori tinyint(4),
-    numero_giocatori_per_squadra tinyint(4),
-    genere char(1),
+    	data_partita timestamp,
+    	anni_classe_partecipanti varchar(1),
+    	max_giocatori tinyint,
+    	min_giocatori tinyint,
+    	numero_giocatori_per_squadra tinyint,
+    	genere varchar(1),
+	id_squadra1 smallint unsigned NOT NULL,
+	id_squadra2 smallint unsigned NOT NULL,
 	id_torneo int REFERENCES tornei(id),
+	FOREIGN KEY (id_squadra1) REFERENCES squadre(id),
+	FOREIGN KEY (id_squadra2) REFERENCES squadre(id),
 	FOREIGN KEY (id_campo) REFERENCES campo(id),
 	FOREIGN KEY (username_creatore) REFERENCES utenti(username)
 
 );
-CREATE TABLE IF NOT EXISTS partite_amatoriali (
+/*CREATE TABLE IF NOT EXISTS partite_amatoriali (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	id_campo smallint(4) UNSIGNED NOT NULL,
 	username_creatore varchar(30) NOT NULL,
 	descrizione varchar(256),
-    data_partita DATE,
-    orario timestamp,
-    max_giocatori tinyint(4),
-    min_giocatori tinyint(4),
-    numero_giocatori_per_squadra tinyint(4),
-    genere char(1),
-    id_torneo int REFERENCES tornei(id),
+    	data_partita timestamp,
+   	max_giocatori tinyint(4),
+    	min_giocatori tinyint(4),
+    	numero_giocatori_per_squadra tinyint(4),
+    	genere varchar(1),
+    	id_torneo int REFERENCES tornei(id),
 	FOREIGN KEY (id_campo) REFERENCES campo(id),
 	FOREIGN KEY (username_creatore) REFERENCES utenti(username)
 
-);
+);*/
 
 -- P: principiante, I: intermedio, A: agonistico
-CREATE TABLE IF NOT EXISTS iscrizioni_sport (
-	id_sport smallint(4) UNSIGNED,
+CREATE TABLE IF NOT EXISTS iscrizioni_utenti_sport (
+	id_sport smallint UNSIGNED,
 	studente varchar(30) NOT NULL,
-	livello_professionalita varchar(1) NOT NULL CHECK (livello_professionalita IN ("P", "I", "A")) -- Da controllare
+	livello_professionalita varchar(1) NOT NULL,
+	FOREIGN KEY (id_sport) REFERENCES sport(id),
+	FOREIGN KEY (studente) REFERENCES utenti(username),
+	PRIMARY KEY(id_sport, studente)
 );
 
-CREATE TABLE IF NOT EXISTS iscrizioni_utenti_tornei (
+CREATE TABLE IF NOT EXISTS iscrizioni_utenti_squadra(
+	username varchar(30) NOT NULL,
+	id_squadra smallint UNSIGNED NOT NULL,
+	FOREIGN KEY (username) REFERENCES utenti(username),
+	FOREIGN KEY (id_squadra) REFERENCES utenti(username),
+	PRIMARY KEY(username, id_squadra)
+);
+
+CREATE TABLE IF NOT EXISTS iscrizioni_squadre_tornei (
 	id_torneo int,
-	username_professore varchar(30) NOT NULL,
+	id_squadra smallint NOT NULL,
 	ruolo varchar(1) NOT NULL,
 	FOREIGN KEY (id_torneo) REFERENCES tornei(id),
-	FOREIGN KEY (username_professore) REFERENCES utenti(username)
+	FOREIGN KEY (id_squadra) REFERENCES squadre(id)
 );
 
-CREATE TABLE IF NOT EXISTS iscrizioni_utenti_tornei (
-	id_torneo int,
-	username_studente varchar(30) NOT NULL,
-	FOREIGN KEY (id_torneo) REFERENCES tornei(id)
-);
